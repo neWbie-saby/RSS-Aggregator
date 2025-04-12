627c70999b8ac3de2cd53bc17c5693d6a55b0900
neWbie-saby <dsabyasachi388@gmail.com>
2025-04-12 10:53:41 +0530

    Added Auth check with API Key logic

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/neWbie-saby/rss-aggregator/internal/auth"
 	"github.com/neWbie-saby/rss-aggregator/internal/database"
 )
 
@@ -30,7 +31,23 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		Name:      params.Name,
 	})
 	if err != nil {
-		respondWithError(w, 200, fmt.Sprintf("Couldn't create user: %v", err))
+		respondWithError(w, 201, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 200, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
